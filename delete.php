ef8ed74402cef0330acb30e88dda44d95deeefaa
Rivane Rasetiansyah <re@nvll.me>
2024-08-26 16:14:49 +0700

    build: mail submenu
    now the menu is more complete

--- delete.php
+++ delete.php
@@ -21,6 +21,7 @@ require_once './common.php';
 require_once './classes/class_local.php';
 
 $ev = "";
+
 try {
     $pop = new nocc_imap();
 } catch (Exception $ex) {
@@ -33,108 +34,127 @@ try {
 }
 
 $num_messages = $pop->num_msg();
-$url = "action.php?" . NOCC_Session::getUrlGetSession();
+$url_session = NOCC_Session::getUrlGetSession();
+$url = "action.php?{$url_session}";
 $user_prefs = NOCC_Session::getUserPrefs();
-
+$referrer = $_SERVER['HTTP_REFERER'] ?? '';
 // Work out folder and target_folder
 $folder = $_SESSION['nocc_folder'];
-$target_folder = "";
-if (isset($_REQUEST['target_folder']) && $_REQUEST['target_folder'] != $folder)
-    $target_folder = $_REQUEST['target_folder'];
-
-if (isset($_REQUEST['bottom_target_folder']) && $_REQUEST['bottom_target_folder'] != $folder)
-    $bottom_target_folder = $_REQUEST['bottom_target_folder'];
+$target_folder = $_REQUEST['target_folder'] ?? '';
+$bottom_target_folder = $_REQUEST['bottom_target_folder'] ?? '';
 
 if (isset($_REQUEST['only_one'])) {
     $mail = $_REQUEST['mail'];
-    if (isset($_REQUEST['move_mode'])) {
-        if ($target_folder != $folder) {
-            $pop->mail_move($mail, $target_folder);
+    $mark_mode = $_REQUEST['mark_mode'] ?? '';
+    $verbose = '0';
+    $display_images = '0';
+
+    if ($referrer) {
+        $parsedUrl = parse_url($referrer);
+        if (isset($parsedUrl['query'])) {
+            parse_str($parsedUrl['query'], $queryParams);
+            $verbose = $queryParams['verbose'] ?? '0';
+            $display_images = $queryParams['display_images'] ?? '0';
         }
     }
-    if (isset($_REQUEST['copy_mode'])) {
-        if ($target_folder != $folder) {
-            $pop->mail_copy($mail, $target_folder);
+
+    if (isset($_REQUEST['move_mode']) && $target_folder !== $folder) {
+        $pop->mail_move($mail, $target_folder);
+        $mail--;
+    } elseif (isset($_REQUEST['copy_mode']) && $target_folder !== $folder) {
+        $pop->mail_copy($mail, $target_folder);
+    } elseif (isset($_REQUEST['mark_mode'])) {
+        switch ($mark_mode) {
+            case 'unread':
+                $pop->mail_mark_unread($mail);
+                break;
+            case 'flag':
+                $pop->mail_mark_flag($mail);
+                break;
+            case 'unflag':
+                $pop->mail_mark_unflag($mail);
+                break;
         }
-    }
-    if (isset($_REQUEST['delete_mode'])) {
-        // If messages are opened in a new windows, we will reload the opener window
-        // i.e. the one with messages list
+    } elseif (isset($_REQUEST['delete_mode'])) {
         $_SESSION['message_deleted'] = "true";
         $target_folder = $_SESSION['imap_namespace'] . $user_prefs->getTrashFolderName();
-        if (
-            $pop->is_imap()
-            && $user_prefs->getUseTrashFolder()
-            && $_SESSION['nocc_folder'] != $target_folder
-        ) {
+
+        if ($pop->is_imap() && $user_prefs->getUseTrashFolder() && $_SESSION['nocc_folder'] !== $target_folder) {
             $pop->mail_move($mail, $target_folder);
         } else {
             $pop->delete($mail);
         }
-        if ($mail - 1) {
-            $url = "action.php?" . NOCC_Session::getUrlGetSession() . "&action=aff_mail&mail=" . --$mail . "&verbose=0";
-        } else {
-            $url = "action.php?" . NOCC_Session::getUrlGetSession();
-        }
+
+        $mail = max(0, $mail - 1);
     }
+
+    $url = $mark_mode === 'unread' || $mail === 0
+        ? "action.php?{$url_session}"
+        : "action.php?{$url_session}&action=aff_mail&mail={$mail}&verbose={$verbose}&display_images={$display_images}";
 } else {
     $msg_to_forward = '';
     for ($i = $num_messages; $i >= 1; $i--) {
-
         if (isset($_REQUEST['msg-' . $i])) {
-            if (isset($_REQUEST['move_mode'])) {
-                if ($target_folder != $folder) {
-                    $pop->mail_move($i, $target_folder);
-                }
+            if (isset($_REQUEST['move_mode']) && $target_folder != $folder) {
+                $pop->mail_move($i, $target_folder);
             }
-            if (isset($_REQUEST['bottom_move_mode'])) {
-                if ($bottom_target_folder != $folder) {
-                    $pop->mail_move($i, $bottom_target_folder);
-                }
+
+            if (isset($_REQUEST['bottom_move_mode']) && $bottom_target_folder != $folder) {
+                $pop->mail_move($i, $bottom_target_folder);
             }
-            if (isset($_REQUEST['copy_mode'])) {
-                if ($target_folder != $folder) {
-                    $pop->mail_copy($i, $target_folder);
-                }
+
+            if (isset($_REQUEST['copy_mode']) && $target_folder != $folder) {
+                $pop->mail_copy($i, $target_folder);
             }
-            if (isset($_REQUEST['bottom_copy_mode'])) {
-                if ($bottom_target_folder != $folder) {
-                    $pop->mail_copy($i, $bottom_target_folder);
-                }
+
+            if (isset($_REQUEST['bottom_copy_mode']) && $bottom_target_folder != $folder) {
+                $pop->mail_copy($i, $bottom_target_folder);
             }
+
             if (isset($_REQUEST['forward_mode']) || isset($_REQUEST['bottom_forward_mode'])) {
                 $msg_to_forward .= '$' . $i;
             }
+
             if (isset($_REQUEST['delete_mode']) || isset($_REQUEST['bottom_delete_mode'])) {
-                // If messages are opened in a new windows, we will reload the opener window
-                // i.e. the one with messages list
                 $_SESSION['message_deleted'] = "true";
                 $target_folder = $_SESSION['imap_namespace'] . $user_prefs->getTrashFolderName();
-                if (
-                    $pop->is_imap()
-                    && $user_prefs->getUseTrashFolder()
-                    && $_SESSION['nocc_folder'] != $target_folder
-                ) {
+
+                if ($pop->is_imap() && $user_prefs->getUseTrashFolder() && $_SESSION['nocc_folder'] != $target_folder) {
                     $pop->mail_move($i, $target_folder);
                 } else {
                     $pop->delete($i);
                 }
             }
 
-            if (isset($_REQUEST['set_flag']) && $_REQUEST['mark_mode'] == 'read') $pop->mail_mark_read($i);
-            if (isset($_REQUEST['set_flag']) && $_REQUEST['mark_mode'] == 'unread') $pop->mail_mark_unread($i);
-            if (isset($_REQUEST['set_flag']) && $_REQUEST['mark_mode'] == 'flag') $pop->mail_mark_flag($i);
-            if (isset($_REQUEST['set_flag']) && $_REQUEST['mark_mode'] == 'unflag') $pop->mail_mark_unflag($i);
+            $mark_actions = [
+                'set_flag' => $_REQUEST['mark_mode'] ?? '',
+                'bottom_set_flag' => $_REQUEST['bottom_mark_mode'] ?? ''
+            ];
 
-            if (isset($_REQUEST['bottom_set_flag']) && $_REQUEST['bottom_mark_mode'] == 'read') $pop->mail_mark_read($i);
-            if (isset($_REQUEST['bottom_set_flag']) && $_REQUEST['bottom_mark_mode'] == 'unread') $pop->mail_mark_unread($i);
-            if (isset($_REQUEST['bottom_set_flag']) && $_REQUEST['bottom_mark_mode'] == 'flag') $pop->mail_mark_flag($i);
-            if (isset($_REQUEST['bottom_set_flag']) && $_REQUEST['bottom_mark_mode'] == 'unflag') $pop->mail_mark_unflag($i);
+            foreach ($mark_actions as $flag_type => $mode) {
+                if (isset($_REQUEST[$flag_type])) {
+                    switch ($mode) {
+                        case 'read':
+                            $pop->mail_mark_read($i);
+                            break;
+                        case 'unread':
+                            $pop->mail_mark_unread($i);
+                            break;
+                        case 'flag':
+                            $pop->mail_mark_flag($i);
+                            break;
+                        case 'unflag':
+                            $pop->mail_mark_unflag($i);
+                            break;
+                    }
+                }
+            }
         }
     }
+
     if ($msg_to_forward != '') {
         $msg_to_forward = substr($msg_to_forward, 1);
-        $url = 'action.php?' . NOCC_Session::getUrlGetSession() . '&action=forward&mail=' . $msg_to_forward;
+        $url = "action.php?{$url_session}&action=forward&mail={$msg_to_forward}";
     }
 }
 
@@ -150,4 +170,5 @@ if (NoccException::isException($ev)) {
 // Redirect user to index
 // TODO: redirect user to next message
 require_once './utils/proxy.php';
+
 header('Location: ' . $conf->base_url . $url);
--- html/submenu_mail.php
+++ html/submenu_mail.php
@@ -1,29 +1,53 @@
 <!-- start of $Id: submenu_mail.php 2610 2014-04-28 08:48:56Z oheil $ -->
 <?php
-if (!isset($conf->loaded))
-  die('Hacking attempt');
+if (!isset($conf->loaded)) die('Hacking attempt');
 
-$display_images = (isset($_REQUEST['display_images']) && $_REQUEST['display_images'] == 1) ? '1' : '0';
+$url_session = NOCC_Session::getUrlGetSession();
+$display_images = (!empty($_REQUEST['display_images']) && $_REQUEST['display_images'] == 1) ? '1' : '0';
+$msgnum = $content['msgnum'];
 ?>
+
 <div class="submenu">
   <ul>
     <li>
-      <a href="action.php?<?php echo NOCC_Session::getUrlGetSession(); ?>&action=compose"><?php echo convertLang2Html($html_new_msg) ?></a>
+      <a href="action.php?<?php echo $url_session ?>&action=compose"><?php echo convertLang2Html($html_new_msg) ?></a>
     </li>
     <li>
-      <a href="action.php?<?php echo NOCC_Session::getUrlGetSession(); ?>&action=reply&amp;mail=<?php echo $content['msgnum'] ?>&amp;display_images=<?php echo $display_images ?>"><?php echo convertLang2Html($html_reply) ?></a>
+      <a href="action.php?<?php echo $url_session ?>&action=reply&amp;mail=<?php echo $msgnum ?>&amp;display_images=<?php echo $display_images ?>"><?php echo convertLang2Html($html_reply) ?></a>
     </li>
     <li>
-      <a href="action.php?<?php echo NOCC_Session::getUrlGetSession(); ?>&action=reply_all&amp;mail=<?php echo $content['msgnum'] ?>&amp;display_images=<?php echo $display_images ?>"><?php echo convertLang2Html($html_reply_all) ?></a>
+      <a href="action.php?<?php echo $url_session ?>&action=reply_all&amp;mail=<?php echo $msgnum ?>&amp;display_images=<?php echo $display_images ?>"><?php echo convertLang2Html($html_reply_all) ?></a>
     </li>
     <li>
-      <a href="action.php?<?php echo NOCC_Session::getUrlGetSession(); ?>&action=forward&amp;mail=<?php echo $content['msgnum'] ?>"><?php echo convertLang2Html($html_forward) ?></a>
+      <a href="delete.php?<?php echo $url_session ?>&mark_mode=unread&amp;mail=<?php echo $msgnum ?>&amp;only_one=1"><?php echo convertLang2Html($html_unread) ?></a>
     </li>
     <li>
-      <a href="down_mail.php?<?php echo NOCC_Session::getUrlGetSession(); ?>&mail=<?php echo $content['msgnum'] ?>"><?php echo convertLang2Html($html_down_mail) ?></a>
+      <a href="delete.php?<?php echo $url_session ?>&mark_mode=flag&amp;mail=<?php echo $msgnum ?>&amp;only_one=1"><?php echo convertLang2Html($html_flag) ?></a>
     </li>
     <li>
-      <a href="delete.php?<?php echo NOCC_Session::getUrlGetSession(); ?>&delete_mode=1&amp;mail=<?php echo $content['msgnum'] ?>&amp;only_one=1" onclick="if (confirm('<?php echo $html_del_msg ?>')) return true; else return false;"><?php echo convertLang2Html($html_delete) ?></a>
+      <a href="delete.php?<?php echo $url_session ?>&mark_mode=unflag&amp;mail=<?php echo $msgnum ?>&amp;only_one=1"><?php echo convertLang2Html($html_unflag) ?></a>
+    </li>
+    <li>
+      <a href="action.php?<?php echo $url_session ?>&action=forward&amp;mail=<?php echo $msgnum ?>"><?php echo convertLang2Html($html_forward) ?></a>
+    </li>
+    <li>
+      <?php if ($pop->is_imap() && $pop->get_folder_count() > 1) {
+        $html_target_select = $pop->html_folder_select('target_folder', '');
+      ?>
+        <form action="delete.php" method="POST">
+          <input type="hidden" name="mail" value="<?php echo $msgnum; ?>" />
+          <input type="hidden" name="only_one" value="1" />
+          <input type="submit" class="button" name="move_mode" value="<?php echo convertLang2Html($html_move); ?>" />
+          <input type="submit" class="button" name="copy_mode" value="<?php echo convertLang2Html($html_copy); ?>" />
+          <?php echo $html_target_select; ?>
+        </form>
+      <?php } ?>
+    </li>
+    <li>
+      <a href="down_mail.php?<?php echo $url_session ?>&mail=<?php echo $msgnum ?>"><?php echo convertLang2Html($html_down_mail) ?></a>
+    </li>
+    <li>
+      <a href="delete.php?<?php echo $url_session ?>&delete_mode=1&amp;mail=<?php echo $msgnum ?>&amp;only_one=1" onclick="return confirm('<?php echo $html_del_msg ?>');"><?php echo convertLang2Html($html_delete) ?></a>
     </li>
   </ul>
 </div>
