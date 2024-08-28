efea957d12345f9fe8a3f68dcb1cf5c4afd1ce0e
Rivane Rasetiansyah <re@nvll.me>
2024-08-28 13:05:26 +0700

    fix: NVLL_RequestTest
    OK (3 tests, 28 assertions)

--- classes/NVLL_Request.php
+++ classes/NVLL_Request.php
@@ -1,24 +1,21 @@
 <?php
+
 /**
  * Class for wrapping the $_REQUEST array
  *
  * Copyright 2010-2011 Tim Gerundt <tim@gerundt.de>
+ * Copyright 2024 Rivane Rasetiansyah <re@nvll.me>
  *
- * This file is part of NOCC. NOCC is free software under the terms of the
+ * This file is part of NVLL. NVLL is free software under the terms of the
  * GNU General Public License. You should have received a copy of the license
- * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package    NOCC
- * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: nocc_request.php 2929 2021-02-02 21:13:19Z oheil $
+ * along with NVLL. If not, see <http://www.gnu.org/licenses>.
  */
 
 /**
  * Wrapping the $_REQUEST array
- *
- * @package    NOCC
  */
-class NOCC_Request {
+class NVLL_Request
+{
     /**
      * Get a string value from the request
      * @param string $key Key
@@ -26,7 +23,8 @@ class NOCC_Request {
      * @return string Value
      * @static
      */
-    public static function getStringValue($key, $defaultValue = '') {
+    public static function getStringValue($key, $defaultValue = '')
+    {
         if (isset($_REQUEST[$key])) {
             //if (get_magic_quotes_gpc()) {  // returns always false since php 5.4
             //    return stripslashes($_REQUEST[$key]);
@@ -43,20 +41,22 @@ class NOCC_Request {
      * @return bool Value
      * @static
      */
-    public static function getBoolValue($key, $defaultValue = false) {
+    public static function getBoolValue($key, $defaultValue = false)
+    {
         if (isset($_REQUEST[$key])) {
-            return NOCC_Request::convertToBool($_REQUEST[$key]);
+            return NVLL_Request::convertToBool($_REQUEST[$key]);
         }
-        return NOCC_Request::convertToBool($defaultValue);
+        return NVLL_Request::convertToBool($defaultValue);
     }
-    
+
     /**
      * Convert value to bool
      * @param mixed $value Value
      * @return bool Bool value
      * @static
      */
-    public static function convertToBool($value) {
+    public static function convertToBool($value)
+    {
         if ($value === true || $value === 1 || strtolower($value) === 'true' || $value === '1') {
             return true;
         }
