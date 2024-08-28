51c37b7f6426a199b4fec3c92164219018ea64c0
Rivane Rasetiansyah <re@nvll.me>
2024-08-28 12:13:38 +0700

    fix: NVLL_EncodingTest
    OK (1 test, 8 assertions)

--- _tests/classes/NVLL_EncodingTest.php
+++ _tests/classes/NVLL_EncodingTest.php
@@ -1,64 +1,60 @@
 <?php
+
 /**
- * Test cases for NOCC_Encoding.
+ * Test cases for NVLL_Encoding.
  *
  * Copyright 2010-2011 Tim Gerundt <tim@gerundt.de>
+ * Copyright 2024 Rivane Rasetiansyah <re@nvll.me>
  *
- * This file is part of NOCC. NOCC is free software under the terms of the
+ * This file is part of NVLL. NVLL is free software under the terms of the
  * GNU General Public License. You should have received a copy of the license
- * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package    NOCC
- * @subpackage Tests
- * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: NOCC_EncodingTest.php 2373 2011-01-04 15:06:58Z gerundt $
+ * along with NVLL. If not, see <http://www.gnu.org/licenses>.
  */
 
-require_once 'PHPUnit/Framework.php';
-
-require_once dirname(__FILE__).'/../../classes/nocc_encoding.php';
+require_once dirname(__FILE__) . '/../../classes/NVLL_Encoding.php';
 
 /**
- * Test class for NOCC_Encoding.
+ * Test class for NVLL_Encoding.
  */
-class NOCC_EncodingTest extends PHPUnit_Framework_TestCase {
+class NVLL_EncodingTest extends PHPUnit\Framework\TestCase
+{
     /**
-     * @var NOCC_Encoding
+     * @var NVLL_Encoding
      */
     protected $encodingNull;
 
     /**
-     * @var NOCC_Encoding
+     * @var NVLL_Encoding
      */
     protected $encodingBug;
 
     /**
-     * @var NOCC_Encoding
+     * @var NVLL_Encoding
      */
     protected $encoding0;
 
     /**
-     * @var NOCC_Encoding
+     * @var NVLL_Encoding
      */
     protected $encoding1;
 
     /**
-     * @var NOCC_Encoding
+     * @var NVLL_Encoding
      */
     protected $encoding2;
 
     /**
-     * @var NOCC_Encoding
+     * @var NVLL_Encoding
      */
     protected $encoding3;
 
     /**
-     * @var NOCC_Encoding
+     * @var NVLL_Encoding
      */
     protected $encoding4;
 
     /**
-     * @var NOCC_Encoding
+     * @var NVLL_Encoding
      */
     protected $encoding5;
 
@@ -66,21 +62,23 @@ class NOCC_EncodingTest extends PHPUnit_Framework_TestCase {
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp() {
-        $this->encodingNull = new NOCC_Encoding(null);
-        $this->encodingBug = new NOCC_Encoding('bug');
-        $this->encoding0 = new NOCC_Encoding(0);
-        $this->encoding1 = new NOCC_Encoding(1);
-        $this->encoding2 = new NOCC_Encoding(2);
-        $this->encoding3 = new NOCC_Encoding(3);
-        $this->encoding4 = new NOCC_Encoding(4);
-        $this->encoding5 = new NOCC_Encoding(5);
+    protected function setUp(): void
+    {
+        $this->encodingNull = new NVLL_Encoding(null);
+        $this->encodingBug = new NVLL_Encoding('bug');
+        $this->encoding0 = new NVLL_Encoding(0);
+        $this->encoding1 = new NVLL_Encoding(1);
+        $this->encoding2 = new NVLL_Encoding(2);
+        $this->encoding3 = new NVLL_Encoding(3);
+        $this->encoding4 = new NVLL_Encoding(4);
+        $this->encoding5 = new NVLL_Encoding(5);
     }
 
     /**
      * Test case for __toString().
      */
-    public function test__toString() {
+    public function test__toString()
+    {
         $this->assertEquals('', $this->encodingNull->__toString(), 'null');
         $this->assertEquals('', $this->encodingBug->__toString(), 'bug');
         $this->assertEquals('7BIT', $this->encoding0->__toString(), '0');
@@ -91,4 +89,3 @@ class NOCC_EncodingTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('OTHER', $this->encoding5->__toString(), '5');
     }
 }
-?>
