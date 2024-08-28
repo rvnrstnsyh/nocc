b404c300571b844ccea4f3dbcf2b705b0caa5384
Rivane Rasetiansyah <re@nvll.me>
2024-08-28 14:33:27 +0700

    fix: NVLL_ThemeTest
    FAILURES!
    Tests: 10, Assertions: 17, Failures: 7, Incomplete: 1.

--- _tests/classes/NVLL_ThemeTest.php
+++ _tests/classes/NVLL_ThemeTest.php
@@ -1,49 +1,46 @@
 <?php
+
 /**
- * Test cases for NOCC_Theme.
+ * Test cases for NVLL_Theme.
  *
  * Copyright 2009-2011 Tim Gerundt <tim@gerundt.de>
+ * Copyright 2024 Rivane Rasetiansyah <re@nvll.me>
  *
- * This file is part of NOCC. NOCC is free software under the terms of the
+ * This file is part of NVLL. NVLL is free software under the terms of the
  * GNU General Public License. You should have received a copy of the license
- * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package    NOCC
- * @subpackage Tests
- * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: NOCC_ThemeTest.php 2373 2011-01-04 15:06:58Z gerundt $
+ * along with NVLL. If not, see <http://www.gnu.org/licenses>.
  */
 
-require_once 'PHPUnit/Framework.php';
-
-require_once dirname(__FILE__).'/../../classes/nocc_theme.php';
+require_once dirname(__FILE__) . '/../../classes/NVLL_Theme.php';
 
 /**
- * Test class for NOCC_Theme.
+ * Test class for NVLL_Theme.
+ * TODO Fix this.
  */
-class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
+class NVLL_ThemeTest extends PHPUnit\Framework\TestCase
+{
     /**
-     * @var NOCC_Theme
+     * @var NVLL_Theme
      */
     protected $theme1;
 
     /**
-     * @var NOCC_Theme
+     * @var NVLL_Theme
      */
     protected $theme2;
 
     /**
-     * @var NOCC_Theme
+     * @var NVLL_Theme
      */
     protected $theme3;
 
     /**
-     * @var NOCC_Theme
+     * @var NVLL_Theme
      */
     protected $theme4;
 
     /**
-     * @var NOCC_Theme
+     * @var NVLL_Theme
      */
     protected $theme5;
 
@@ -51,18 +48,20 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp() {
-        $this->theme1 = new NOCC_Theme('test1');
-        $this->theme2 = new NOCC_Theme('notexists');
-        $this->theme3 = new NOCC_Theme('');
-        $this->theme4 = new NOCC_Theme('../../../../../../../etc/passwd%00');
-        $this->theme5 = new NOCC_Theme('<script>alert(document.cookie)</script>');
+    protected function setUp(): void
+    {
+        $this->theme1 = new NVLL_Theme('test1');
+        $this->theme2 = new NVLL_Theme('notexists');
+        $this->theme3 = new NVLL_Theme('');
+        $this->theme4 = new NVLL_Theme('../../../../../../../etc/passwd%00');
+        $this->theme5 = new NVLL_Theme('<script>alert(document.cookie)</script>');
     }
 
     /**
      * Test case for getName().
      */
-    public function testGetName() {
+    public function testGetName()
+    {
         $this->assertEquals('test1', $this->theme1->getName(), 'test1');
         $this->assertEquals('notexists', $this->theme2->getName(), 'notexists');
         $this->assertEquals('', $this->theme3->getName());
@@ -73,7 +72,8 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getPath().
      */
-    public function testGetPath() {
+    public function testGetPath()
+    {
         $this->assertEquals('themes/test1', $this->theme1->getPath(), 'test1');
         $this->assertEquals('', $this->theme2->getPath(), 'notexists');
         $this->assertEquals('', $this->theme3->getPath());
@@ -84,7 +84,8 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getRealPath().
      */
-    public function testGetRealPath() {
+    public function testGetRealPath()
+    {
         $this->assertNotEquals('', $this->theme1->getRealPath(), 'test1');
         $this->assertEquals('', $this->theme2->getRealPath(), 'notexists');
         $this->assertEquals('', $this->theme3->getRealPath());
@@ -95,7 +96,8 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for exists().
      */
-    public function testExists() {
+    public function testExists()
+    {
         $this->assertTrue($this->theme1->exists(), 'test1');
         $this->assertFalse($this->theme2->exists(), 'notexists');
         $this->assertFalse($this->theme3->exists());
@@ -106,7 +108,8 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getStylesheet().
      */
-    public function testGetStylesheet() {
+    public function testGetStylesheet()
+    {
         $this->assertEquals('themes/test1/style.css', $this->theme1->getStylesheet(), 'test1');
         $this->assertEquals('', $this->theme2->getStylesheet(), 'notexists');
         $this->assertEquals('', $this->theme3->getStylesheet());
@@ -117,7 +120,8 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getPrintStylesheet().
      */
-    public function testGetPrintStylesheet() {
+    public function testGetPrintStylesheet()
+    {
         $this->assertEquals('themes/test1/print.css', $this->theme1->getPrintStylesheet(), 'test1');
         $this->assertEquals('', $this->theme2->getPrintStylesheet(), 'notexists');
         $this->assertEquals('', $this->theme3->getPrintStylesheet());
@@ -128,7 +132,8 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getFavicon().
      */
-    public function testGetFavicon() {
+    public function testGetFavicon()
+    {
         $this->assertEquals('favicon.ico', $this->theme1->getFavicon(), 'test1');
         $this->assertEquals('favicon.ico', $this->theme2->getFavicon(), 'notexists');
         $this->assertEquals('favicon.ico', $this->theme3->getFavicon());
@@ -139,7 +144,8 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getCustomHeader().
      */
-    public function testGetCustomHeader() {
+    public function testGetCustomHeader()
+    {
         $this->assertNotEquals('', $this->theme1->getCustomHeader(), 'test1');
         $this->assertEquals('', $this->theme2->getCustomHeader(), 'notexists');
         $this->assertEquals('', $this->theme3->getCustomHeader());
@@ -150,7 +156,8 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getCustomFooter().
      */
-    public function testGetCustomFooter() {
+    public function testGetCustomFooter()
+    {
         $this->assertNotEquals('', $this->theme1->getCustomFooter(), 'test1');
         $this->assertEquals('', $this->theme2->getCustomFooter(), 'notexists');
         $this->assertEquals('', $this->theme3->getCustomFooter());
@@ -161,11 +168,11 @@ class NOCC_ThemeTest extends PHPUnit_Framework_TestCase {
     /**
      * @todo Implement testReplaceTextSmilies().
      */
-    public function testReplaceTextSmilies() {
+    public function testReplaceTextSmilies()
+    {
         // Remove the following lines when you implement this test.
         $this->markTestIncomplete(
-                'This test has not been implemented yet.'
+            'This test has not been implemented yet.'
         );
     }
 }
-?>
--- classes/NVLL_Theme.php
+++ classes/NVLL_Theme.php
@@ -1,62 +1,60 @@
 <?php
+
 /**
  * Class for wrapping a theme
  *
  * Copyright 2009-2011 Tim Gerundt <tim@gerundt.de>
+ * Copyright 2024 Rivane Rasetiansyah <re@nvll.me>
  *
- * This file is part of NOCC. NOCC is free software under the terms of the
+ * This file is part of NVLL. NVLL is free software under the terms of the
  * GNU General Public License. You should have received a copy of the license
- * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package    NOCC
- * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: nocc_theme.php 3060 2023-03-05 19:06:00Z oheil $
+ * along with NVLL. If not, see <http://www.gnu.org/licenses>.
  */
 
 /**
  * Wrapping details from a theme
- *
- * @package    NOCC
  */
-class NOCC_Theme {
+class NVLL_Theme
+{
     /**
      * Name
      * @var string
      * @access private
      */
     private $_name;
-    
+
     /**
      * Path
      * @var string
      * @access private
      */
     private $_path;
-    
+
     /**
      * Real path
      * @var string
      * @access private
      */
     private $_realpath;
-    
+
     /**
      * Exists?
      * @var bool
      * @access private
      */
     private $_exists;
-    
+
     /**
      * Initialize the theme wrapper
      * @param string $name Theme name
      */
-    public function __construct($name) {
+    public function __construct($name)
+    {
         $this->_name = '';
         $this->_path = '';
         $this->_realpath = '';
         $this->_exists = false;
-        
+
         $name = strip_tags($name);
         $name = str_replace('..', '', $name);
         $name = str_replace('/', '', $name);
@@ -71,102 +69,112 @@ class NOCC_Theme {
             }
         }
     }
-    
+
     /**
      * Get the name from the theme
      * @return string Name
      */
-    public function getName() {
+    public function getName()
+    {
         return $this->_name;
     }
-    
+
     /**
      * Get the path from the theme
      * @return string Path
      */
-    public function getPath() {
+    public function getPath()
+    {
         return $this->_path;
     }
-    
+
     /**
      * Get the real path from the theme
      * @return string Real path
      */
-    public function getRealPath() {
+    public function getRealPath()
+    {
         return $this->_realpath;
     }
-    
+
     /**
      * Exists the theme?
      * @return bool Exists?
      */
-    public function exists() {
+    public function exists()
+    {
         return $this->_exists;
     }
-    
+
     /**
      * Get the stylesheet from the theme
      * @return string Stylesheet
      */
-    public function getStylesheet() {
+    public function getStylesheet()
+    {
         if ($this->_exists) { //if exists...
             return $this->_path . '/style.css';
         }
         return '';
     }
-    
+
     /**
      * Get the print stylesheet from the theme
      * @return string Print stylesheet
      */
-    public function getPrintStylesheet() {
+    public function getPrintStylesheet()
+    {
         if ($this->_exists) { //if exists...
             return $this->_path . '/print.css';
         }
         return '';
     }
-    
+
     /**
      * Get the favicon from the theme
      * @return string Favicon
      */
-    public function getFavicon() {
+    public function getFavicon()
+    {
         if (file_exists($this->_realpath . '/favicon.ico')) //if theme favicon exists...
             return $this->_path . '/favicon.ico';
         else //if NO theme favicon exists...
             return 'favicon.ico';
     }
-    
+
     /**
      * Get the custom header from the theme
      * @return string Custom header
      */
-    public function getCustomHeader() {
+    public function getCustomHeader()
+    {
         if ($this->_exists) { //if exists...
             return $this->_realpath . '/header.php';
         }
         return '';
     }
-    
+
     /**
      * Get the custom footer from the theme
      * @return string Custom footer
      */
-    public function getCustomFooter() {
+    public function getCustomFooter()
+    {
         if ($this->_exists) { //if exists...
             return $this->_realpath . '/footer.php';
         }
         return '';
     }
-    
+
     /**
      * Replace text smilies with graphical smilies
      * @param string $body String with text smilies
      * @return string String with graphical smilies (HTML)
      */
-    public function replaceTextSmilies($body) {
+    public function replaceTextSmilies($body)
+    {
         $smiliespath = $this->_path . '/img/smilies';
-        
+
         $body = preg_replace('|\;\-?\)|', '<img src="' . $smiliespath . '/wink.png" alt="wink"/>', $body); // ;-) ;)
         $body = preg_replace('|\;\-?D|', '<img src="' . $smiliespath . '/grin.png" alt="grin"/>', $body); // ;-D ;D
         $body = preg_replace('|:\'\(?|', '<img src="' . $smiliespath . '/cry.png" alt="cry"/>', $body); // :'( :'
@@ -178,7 +186,7 @@ class NOCC_Theme {
         $body = preg_replace('|:-\?\(|', '<img src="' . $smiliespath . '/unhappy.png" alt="unhappy"/>', $body); // :-( :(
         $body = preg_replace('|:\-O|i', '<img src="' . $smiliespath . '/surprised.png" alt="surprised"/>', $body); // :-o :-O
         $body = preg_replace('|8\-?\)|', '<img src="' . $smiliespath . '/cool.png" alt="cool"/>', $body); // 8-) 8)
-        
+
         return $body;
     }
 }
