6868afe38acd041dbb0323e65f9d9bf5001db291
Rivane Rasetiansyah <re@nvll.me>
2024-08-28 14:23:08 +0700

    fix: NVLL_ThemesTest
    OK (7 tests, 56 assertions)

--- _tests/classes/NVLL_ThemesTest.php
+++ _tests/classes/NVLL_ThemesTest.php
@@ -1,54 +1,50 @@
 <?php
+
 /**
- * Test cases for NOCC_Themes.
+ * Test cases for NVLL_Themes.
  *
  * Copyright 2009-2011 Tim Gerundt <tim@gerundt.de>
+ * Copyright 2024 Rivane Rasetiansyah <re@nvll.me>
  *
- * This file is part of NOCC. NOCC is free software under the terms of the
+ * This file is part of NVLL. NVLL is free software under the terms of the
  * GNU General Public License. You should have received a copy of the license
- * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package    NOCC
- * @subpackage Tests
- * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: NOCC_ThemesTest.php 2373 2011-01-04 15:06:58Z gerundt $
+ * along with NVLL. If not, see <http://www.gnu.org/licenses>.
  */
 
-require_once 'PHPUnit/Framework.php';
-
-require_once dirname(__FILE__).'/../../classes/nocc_themes.php';
+require_once dirname(__FILE__) . '/../../classes/NVLL_Themes.php';
 
 /**
- * Test class for NOCC_Themes.
+ * Test class for NVLL_Themes.
  */
-class NOCC_ThemesTest extends PHPUnit_Framework_TestCase {
+class NVLL_ThemesTest extends PHPUnit\Framework\TestCase
+{
     /**
      * @var string
      */
     protected $rootPath;
 
     /**
-     * @var NOCC_Themes
+     * @var NVLL_Themes
      */
     protected $themes1;
 
     /**
-     * @var NOCC_Themes
+     * @var NVLL_Themes
      */
     protected $themes2;
 
     /**
-     * @var NOCC_Themes
+     * @var NVLL_Themes
      */
     protected $themes3;
 
     /**
-     * @var NOCC_Themes
+     * @var NVLL_Themes
      */
     protected $themes4;
 
     /**
-     * @var NOCC_Themes
+     * @var NVLL_Themes
      */
     protected $themes5;
 
@@ -56,20 +52,22 @@ class NOCC_ThemesTest extends PHPUnit_Framework_TestCase {
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp() {
+    protected function setUp(): void
+    {
         $this->rootPath = dirname(__FILE__) . '/../';
 
-        $this->themes1 = new NOCC_Themes('');
-        $this->themes2 = new NOCC_Themes($this->rootPath . './themes', 'test1');
-        $this->themes3 = new NOCC_Themes($this->rootPath . './themes/', 'TEST1');
-        $this->themes4 = new NOCC_Themes(array('bug'));
-        $this->themes5 = new NOCC_Themes($this->rootPath . './themes/', array('bug'));
+        $this->themes1 = new NVLL_Themes('');
+        $this->themes2 = new NVLL_Themes($this->rootPath . './themes', 'test1');
+        $this->themes3 = new NVLL_Themes($this->rootPath . './themes/', 'TEST1');
+        $this->themes4 = new NVLL_Themes(array('bug'));
+        $this->themes5 = new NVLL_Themes($this->rootPath . './themes/', array('bug'));
     }
 
     /**
      * Test case for count().
      */
-    public function testCount() {
+    public function testCount()
+    {
         $this->assertEquals(0, $this->themes1->count());
         $this->assertEquals(2, $this->themes2->count(), './themes, test1');
         $this->assertEquals(2, $this->themes3->count(), './themes/ TEST1');
@@ -80,10 +78,11 @@ class NOCC_ThemesTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for exists().
      */
-    public function testExists() {
-        $themes = new NOCC_Themes($this->rootPath . './themes');
+    public function testExists()
+    {
+        $themes = new NVLL_Themes($this->rootPath . './themes');
 
-        $this->assertFalse(@$themes->exists(), 'exists()');
+        $this->assertFalse(@$themes->exists(''), 'exists()');
         $this->assertFalse($themes->exists(array('bug')), 'exists(array("bug"))');
         $this->assertFalse($themes->exists(''), 'exists("")');
         $this->assertFalse($themes->exists('notexists'), 'exists("notexists")');
@@ -94,8 +93,9 @@ class NOCC_ThemesTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getThemeNames().
      */
-    public function testGetThemeNames() {
-        $themes = new NOCC_Themes($this->rootPath . './themes');
+    public function testGetThemeNames()
+    {
+        $themes = new NVLL_Themes($this->rootPath . './themes');
         $themeNames = $themes->getThemeNames();
 
         $this->assertEquals(2, count($themeNames), 'count($themeNames)');
@@ -106,32 +106,34 @@ class NOCC_ThemesTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getDefaultThemeName().
      */
-    public function testGetDefaultThemeName() {
-        $this->assertEquals('standard', $this->themes1->getDefaultThemeName());
+    public function testGetDefaultThemeName()
+    {
+        $this->assertEquals('default', $this->themes1->getDefaultThemeName());
         $this->assertEquals('test1', $this->themes2->getDefaultThemeName(), './themes, test1');
         $this->assertEquals('test1', $this->themes3->getDefaultThemeName(), './themes/ TEST1');
-        $this->assertEquals('standard', $this->themes4->getDefaultThemeName(), 'array(bug)');
-        $this->assertEquals('standard', $this->themes5->getDefaultThemeName(), './themes/, array(bug)');
+        $this->assertEquals('default', $this->themes4->getDefaultThemeName(), 'array(bug)');
+        $this->assertEquals('default', $this->themes5->getDefaultThemeName(), './themes/, array(bug)');
     }
 
     /**
      * Test case for setDefaultThemeName().
      */
-    public function testSetDefaultThemeName() {
-        $themes = new NOCC_Themes($this->rootPath . './themes');
+    public function testSetDefaultThemeName()
+    {
+        $themes = new NVLL_Themes($this->rootPath . './themes');
 
-        $this->assertFalse(@$themes->setDefaultThemeName(), 'setDefaultThemeName()');
-        $this->assertEquals('standard', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
+        $this->assertFalse(@$themes->setDefaultThemeName(''), 'setDefaultThemeName()');
+        $this->assertEquals('default', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
         $this->assertFalse($themes->setDefaultThemeName(array('bug')), 'setDefaultThemeName(array("bug"))');
-        $this->assertEquals('standard', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
+        $this->assertEquals('default', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
         $this->assertFalse($themes->setDefaultThemeName(''), 'setDefaultThemeName("")');
-        $this->assertEquals('standard', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
+        $this->assertEquals('default', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
         $this->assertFalse($themes->setDefaultThemeName('notexists'), 'setDefaultThemeName("notexists")');
-        $this->assertEquals('standard', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
+        $this->assertEquals('default', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
         $this->assertFalse($themes->setDefaultThemeName('../../../../../../../etc/passwd%00'), 'setDefaultThemeName("passwd")');
-        $this->assertEquals('standard', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
+        $this->assertEquals('default', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
         $this->assertFalse($themes->setDefaultThemeName('<script>alert(document.cookie)</script>'), 'setDefaultThemeName("alert()")');
-        $this->assertEquals('standard', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
+        $this->assertEquals('default', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
         $this->assertTrue($themes->setDefaultThemeName('test1'), 'setDefaultThemeName("test1")');
         $this->assertEquals('test1', $themes->getDefaultThemeName(), 'getDefaultThemeName()');
         $this->assertTrue($themes->setDefaultThemeName('TEST2'), 'setDefaultThemeName("TEST2")');
@@ -141,36 +143,37 @@ class NOCC_ThemesTest extends PHPUnit_Framework_TestCase {
     /**
      * Test case for getSelectedThemeName().
      */
-    public function testGetSelectedThemeName() {
-        $this->assertEquals('standard', $this->themes1->getSelectedThemeName());
+    public function testGetSelectedThemeName()
+    {
+        $this->assertEquals('default', $this->themes1->getSelectedThemeName());
         $this->assertEquals('test1', $this->themes2->getSelectedThemeName(), './themes, test1');
         $this->assertEquals('test1', $this->themes3->getSelectedThemeName(), './themes/ TEST1');
-        $this->assertEquals('standard', $this->themes4->getSelectedThemeName(), 'array(bug)');
-        $this->assertEquals('standard', $this->themes5->getSelectedThemeName(), './themes/, array(bug)');
+        $this->assertEquals('default', $this->themes4->getSelectedThemeName(), 'array(bug)');
+        $this->assertEquals('default', $this->themes5->getSelectedThemeName(), './themes/, array(bug)');
     }
 
     /**
      * Test case for setSelectedThemeName().
      */
-    public function testSetSelectedThemeName() {
-        $themes = new NOCC_Themes($this->rootPath . './themes');
+    public function testSetSelectedThemeName()
+    {
+        $themes = new NVLL_Themes($this->rootPath . './themes');
 
-        $this->assertFalse(@$themes->setSelectedThemeName(), 'setSelectedThemeName()');
-        $this->assertEquals('standard', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
+        $this->assertFalse(@$themes->setSelectedThemeName(''), 'setSelectedThemeName()');
+        $this->assertEquals('default', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
         $this->assertFalse($themes->setSelectedThemeName(array('bug')), 'setSelectedThemeName(array("bug"))');
-        $this->assertEquals('standard', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
+        $this->assertEquals('default', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
         $this->assertFalse($themes->setSelectedThemeName(''), 'setSelectedThemeName("")');
-        $this->assertEquals('standard', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
+        $this->assertEquals('default', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
         $this->assertFalse($themes->setSelectedThemeName('../../../../../../../etc/passwd%00'), 'setSelectedThemeName("passwd")');
-        $this->assertEquals('standard', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
+        $this->assertEquals('default', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
         $this->assertFalse($themes->setSelectedThemeName('<script>alert(document.cookie)</script>'), 'setSelectedThemeName("alert()")');
-        $this->assertEquals('standard', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
+        $this->assertEquals('default', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
         $this->assertFalse($themes->setSelectedThemeName('notexists'), 'setSelectedThemeName("notexists")');
-        $this->assertEquals('standard', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
+        $this->assertEquals('default', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
         $this->assertTrue($themes->setSelectedThemeName('test1'), 'setSelectedThemeName("test1")');
         $this->assertEquals('test1', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
         $this->assertTrue($themes->setSelectedThemeName('TEST2'), 'setSelectedThemeName("TEST2")');
         $this->assertEquals('test2', $themes->getSelectedThemeName(), 'getSelectedThemeName()');
     }
 }
-?>
--- classes/NVLL_Themes.php
+++ classes/NVLL_Themes.php
@@ -1,24 +1,21 @@
 <?php
+
 /**
  * Class for wrapping the themes
  *
  * Copyright 2009-2011 Tim Gerundt <tim@gerundt.de>
+ * Copyright 2024 Rivane Rasetiansyah <re@nvll.me>
  *
- * This file is part of NOCC. NOCC is free software under the terms of the
+ * This file is part of NVLL. NVLL is free software under the terms of the
  * GNU General Public License. You should have received a copy of the license
- * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package    NOCC
- * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: nocc_themes.php 3016 2022-08-25 11:00:42Z oheil $
+ * along with NVLL. If not, see <http://www.gnu.org/licenses>.
  */
 
 /**
  * Wrapping the themes
- *
- * @package    NOCC
  */
-class NOCC_Themes {
+class NVLL_Themes
+{
     /**
      * Themes
      * @var array
@@ -45,7 +42,8 @@ class NOCC_Themes {
      * @param string $path Themes path (relative)
      * @param string $defaultThemeName Default theme name
      */
-    public function __construct($path, $defaultThemeName = '') {
+    public function __construct($path, $defaultThemeName = '')
+    {
         $this->_themes = array();
         $this->_defaultThemeName = 'default';
         $this->_selectedThemeName = '';
@@ -53,10 +51,10 @@ class NOCC_Themes {
         if (isset($path) && is_string($path) && !empty($path)) { //if path is set...
             if (is_dir($path)) { //if is directory...
                 if (substr($path, -1) != '/') { //if NOT ends with '/'...
-                  $path .= '/';
+                    $path .= '/';
                 }
-                
-                //TODO: Move some code to a NOCC_Directory class?
+
+                //TODO: Move some code to a NVLL_Directory class?
                 if ($handle = opendir($path)) { //if can open the directory...
                     while (false !== ($name = readdir($handle))) { //for each item...
                         if ($name != '.' && $name != '..' && is_dir($path . $name)) { //if subdirectory...
@@ -79,19 +77,21 @@ class NOCC_Themes {
      * Get the count from the themes
      * @return int Count
      */
-    public function count() {
+    public function count()
+    {
         return count($this->_themes);
     }
-    
+
     /**
      * Exists the theme?
      * @param string $themeName Theme name
      * @return bool Exists?
      */
-    public function exists($themeName) {
+    public function exists($themeName)
+    {
         if (isset($themeName) && is_string($themeName) && !empty($themeName)) { //if theme name is set...
             $themeName = strtolower($themeName);
-            
+
             return array_key_exists($themeName, $this->_themes);
         }
         return false;
@@ -101,7 +101,8 @@ class NOCC_Themes {
      * Get the theme names
      * @return array Theme names
      */
-    public function getThemeNames() {
+    public function getThemeNames()
+    {
         return array_keys($this->_themes);
     }
 
@@ -109,7 +110,8 @@ class NOCC_Themes {
      * Get the default theme name
      * @return string Default theme name
      */
-    public function getDefaultThemeName() {
+    public function getDefaultThemeName()
+    {
         return $this->_defaultThemeName;
     }
 
@@ -118,7 +120,8 @@ class NOCC_Themes {
      * @param string $themeName Default theme name
      * @return bool Successful?
      */
-    public function setDefaultThemeName($themeName) {
+    public function setDefaultThemeName($themeName)
+    {
         if ($this->exists($themeName)) { //if the theme exists...
             $this->_defaultThemeName = strtolower($themeName);
             return true;
@@ -130,7 +133,8 @@ class NOCC_Themes {
      * Get the selected theme name
      * @return string Selected theme name
      */
-    public function getSelectedThemeName() {
+    public function getSelectedThemeName()
+    {
         if (!empty($this->_selectedThemeName)) { //if a theme is selected...
             return $this->_selectedThemeName;
         }
@@ -142,7 +146,8 @@ class NOCC_Themes {
      * @param string $themeName Selected theme name
      * @return bool Successful?
      */
-    public function setSelectedThemeName($themeName) {
+    public function setSelectedThemeName($themeName)
+    {
         if ($this->exists($themeName)) { //if the theme exists...
             $this->_selectedThemeName = strtolower($themeName);
             return true;
