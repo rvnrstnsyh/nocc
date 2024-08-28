a1f440e70979568f1d2a00f31880d51e330cb3a0
Rivane Rasetiansyah <re@nvll.me>
2024-08-28 13:12:19 +0700

    fix: NVLL_RSSFeedTest
    OK, but incomplete, skipped, or risky tests!
    Tests: 9, Assertions: 6, Incomplete: 5.

--- _tests/classes/NVLL_RSSFeedTest.php
+++ _tests/classes/NVLL_RSSFeedTest.php
@@ -1,29 +1,25 @@
 <?php
+
 /**
- * Test cases for NOCC_RssFeed.
+ * Test cases for NVLL_RSSFeed.
  *
  * Copyright 2010-2011 Tim Gerundt <tim@gerundt.de>
+ * Copyright 2024 Rivane Rasetiansyah <re@nvll.me>
  *
- * This file is part of NOCC. NOCC is free software under the terms of the
+ * This file is part of NVLL. NVLL is free software under the terms of the
  * GNU General Public License. You should have received a copy of the license
- * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package    NOCC
- * @subpackage Tests
- * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: NOCC_RssFeedTest.php 2373 2011-01-04 15:06:58Z gerundt $
+ * along with NVLL. If not, see <http://www.gnu.org/licenses>.
  */
 
-require_once 'PHPUnit/Framework.php';
-
-require_once dirname(__FILE__).'/../../classes/nocc_rssfeed.php';
+require_once dirname(__FILE__) . '/../../classes/NVLL_RSSfeed.php';
 
 /**
- * Test class for NOCC_RssFeed.
+ * Test class for NVLL_RSSFeed.
  */
-class NOCC_RssFeedTest extends PHPUnit_Framework_TestCase {
+class NVLL_RSSFeedTest extends PHPUnit\Framework\TestCase
+{
     /**
-     * @var NOCC_RssFeed
+     * @var NVLL_RSSFeed
      */
     protected $rssFeed1;
 
@@ -31,94 +27,103 @@ class NOCC_RssFeedTest extends PHPUnit_Framework_TestCase {
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp() {
-        $this->rssFeed1 = new NOCC_RssFeed;
+    protected function setUp(): void
+    {
+        $this->rssFeed1 = new NVLL_RSSFeed;
         $this->rssFeed1->setTitle('Title');
         $this->rssFeed1->setDescription('Description');
-        $this->rssFeed1->setLink('http://nocc.sf.net/');
+        $this->rssFeed1->setLink('http://nvll.sf.net/');
     }
 
     /**
      * Test case for getTitle().
      */
-    public function testGetTitle() {
+    public function testGetTitle()
+    {
         $this->assertEquals('Title', $this->rssFeed1->getTitle());
     }
 
     /**
      * @todo Implement testSetTitle().
      */
-    public function testSetTitle() {
+    public function testSetTitle()
+    {
         // Remove the following lines when you implement this test.
         $this->markTestIncomplete(
-                'This test has not been implemented yet.'
+            'This test has not been implemented yet.'
         );
     }
 
     /**
      * Test case for getDescription().
      */
-    public function testGetDescription() {
+    public function testGetDescription()
+    {
         $this->assertEquals('Description', $this->rssFeed1->getDescription());
     }
 
     /**
      * @todo Implement testSetDescription().
      */
-    public function testSetDescription() {
+    public function testSetDescription()
+    {
         // Remove the following lines when you implement this test.
         $this->markTestIncomplete(
-                'This test has not been implemented yet.'
+            'This test has not been implemented yet.'
         );
     }
 
     /**
      * Test case for getLink().
      */
-    public function testGetLink() {
-        $this->assertEquals('http://nocc.sf.net/', $this->rssFeed1->getLink());
+    public function testGetLink()
+    {
+        $this->assertEquals('http://nvll.sf.net/', $this->rssFeed1->getLink());
     }
 
     /**
      * @todo Implement testSetLink().
      */
-    public function testSetLink() {
+    public function testSetLink()
+    {
         // Remove the following lines when you implement this test.
         $this->markTestIncomplete(
-                'This test has not been implemented yet.'
+            'This test has not been implemented yet.'
         );
     }
 
     /**
      * @todo Implement testAddItem().
      */
-    public function testAddItem() {
+    public function testAddItem()
+    {
         // Remove the following lines when you implement this test.
         $this->markTestIncomplete(
-                'This test has not been implemented yet.'
+            'This test has not been implemented yet.'
         );
     }
 
     /**
      * @todo Implement testSendToBrowser().
      */
-    public function testSendToBrowser() {
+    public function testSendToBrowser()
+    {
         // Remove the following lines when you implement this test.
         $this->markTestIncomplete(
-                'This test has not been implemented yet.'
+            'This test has not been implemented yet.'
         );
     }
 
     /**
      * Test case for getIso8601Date().
      */
-    public function testGetIso8601Date() {
+    public function testGetIso8601Date()
+    {
         $timestamp = mktime(22, 26, 11, 2, 19, 2010);
         $tzd = substr(date('O', $timestamp), 0, 3) . ':' . substr(date('O', $timestamp), -2);
 
-        $this->assertStringStartsWith('2010-02-19T22:26:11', NOCC_RssFeed::getIso8601Date($timestamp), 'starts with');
-        $this->assertStringEndsWith($tzd, NOCC_RssFeed::getIso8601Date($timestamp), 'ends with');
-        $this->assertEquals(25, strlen(NOCC_RssFeed::getIso8601Date($timestamp)), 'length');
+        $this->assertStringStartsWith('2010-02-19T22:26:11', NVLL_RSSFeed::getIso8601Date($timestamp), 'starts with');
+        $this->assertStringEndsWith($tzd, NVLL_RSSFeed::getIso8601Date($timestamp), 'ends with');
+        $this->assertEquals(25, strlen(NVLL_RSSFeed::getIso8601Date($timestamp)), 'length');
     }
 }
-?>
